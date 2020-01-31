9bef78d7e4ece7cae7b081e9b9c7865b4600d487
Nouman Ahmad <noumanahmad@hotmail.com>
2020-01-31 18:56:00 +0500

    dev environments set

--- truffle.js
+++ truffle.js
@@ -5,13 +5,8 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 7545,
+      port: 8545,
       network_id: "*" // Match any network id
-    },
-    test: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "5777"
     }
   }
 };
